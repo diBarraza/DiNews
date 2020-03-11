b39bc1ce00c8ce2f2c2c17c48b1623ab1826a485
Diego <dbarraza@disc.ucn.cl>
2020-03-11 03:20:49 -0300

    feat(finish part 1 tutorial theNews): the aplication is compiled but dont start in simulator android
    this code sucessful the test and process of compile, but dont see any in
    simulator of androd , the error is in mainActivity of aplication , see
    the error in the log.
    
    BREAKING CHANGE: Todo: fix the error for simulator

--- app/src/main/java/cl/ucn/disc/dsm/dinews/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/dinews/activities/MainActivity.java
@@ -1,11 +1,25 @@
 package cl.ucn.disc.dsm.dinews.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import cl.ucn.disc.dsm.dinews.R;
+import cl.ucn.disc.dsm.dinews.activities.adapters.NoticiaAdapter;
 import cl.ucn.disc.dsm.dinews.databinding.ActivityMainBinding;
+import cl.ucn.disc.dsm.dinews.model.Noticia;
+import cl.ucn.disc.dsm.dinews.services.NewsApi.NewsApiNoticiaService;
+import cl.ucn.disc.dsm.dinews.services.NoticiaService;
+
+import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * The Main Launcher Activity.
@@ -15,6 +29,15 @@ import org.slf4j.LoggerFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The Adapter
+     */
+    private NoticiaAdapter noticiaAdapter;
+
+    /**
+     * The NoticiaService
+     */
+    private NewsApiNoticiaService newsApiNoticiaService;
     /**
      * The Logger
      */
@@ -42,8 +65,78 @@ public class MainActivity extends AppCompatActivity {
         {
             binding.swlRefresh.setOnRefreshListener(() -> {
                 log.debug("Refreshing ..");
+
+                // Execute in background ..
+                AsyncTask.execute(() -> {
+
+                    // How much time do we need?
+                    final StopWatch stopWatch = StopWatch.createStarted();
+
+                    try {
+
+                        // 1. Get the List from NewsApi (in background)
+                        final List<Noticia> noticias = this.newsApiNoticiaService.getNoticias(50);
+
+                        // (in UI)
+                        this.runOnUiThread(() -> {
+
+                            // 2. Set in the adapter (
+                            this.noticiaAdapter.setNoticias(noticias);
+
+                            // 3. Show a Toast!
+                            Toast.makeText(this, "Done: " + stopWatch, Toast.LENGTH_SHORT).show();
+
+                        });
+
+                    } catch (Exception ex) {
+
+                        log.error("Error", ex);
+
+                        // (in UI)
+                        this.runOnUiThread(() -> {
+
+                            // Build the message
+                            final StringBuffer sb = new StringBuffer("Error: ");
+                            sb.append(ex.getMessage());
+                            if (ex.getCause() != null) {
+                                sb.append(", ");
+                                sb.append(ex.getCause().getMessage());
+                            }
+
+                            // 3. Show the Toast!
+                            Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG).show();
+
+                        });
+
+                    } finally {
+
+                        // 4. Hide the spinning circle
+                        binding.swlRefresh.setRefreshing(false);
+
+                    }
+
+                });
+
             });
         }
+
+        // The Adapter + RecyclerView
+        {
+            // The Adapter
+            this.noticiaAdapter = new NoticiaAdapter();
+
+            // The Adapter
+            binding.rvNoticias.setAdapter(this.noticiaAdapter);
+
+            // The layout (ListView)
+            binding.rvNoticias.setLayoutManager(new LinearLayoutManager(this));
+
+            // The separator (line)
+            binding.rvNoticias.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        }
+
+        // The NoticiaService
+        this.newsApiNoticiaService = new NewsApiNoticiaService();
 
     }
 }
